80f7cd3047f73198d9fd58cb05d4e3f3024b04ec
Zishu Zhou <zszhoucs@icloud.com>
2025-09-05 23:47:30 +0800

    add TextCNN

---
+++ models/TextCNN.py
@@ -0,0 +1,53 @@
+import torch
+import torch.nn.functional as F
+from torch import nn
+
+
+class TextCNN(nn.Module):
+    def __init__(self, hidden_dim, num_channels=100, kernel_size=[3,4,5], max_len=200, dropout=0.8, 
+                padding_idx=0, vocab_size=98635, num_classes=10):
+        super(TextCNN, self).__init__()
+        
+        # Embedding Layer
+        self.embedding = nn.Embedding(vocab_size, hidden_dim, padding_idx)
+        
+        # This stackoverflow thread clarifies how conv1d works
+        # https://stackoverflow.com/questions/46503816/keras-conv1d-layer-parameters-filters-and-kernel-size/46504997
+        self.conv1 = nn.Sequential(
+            nn.Conv1d(in_channels=hidden_dim, out_channels=num_channels, kernel_size=kernel_size[0]),
+            nn.ReLU(),
+            nn.MaxPool1d(max_len - kernel_size[0]+1)
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv1d(in_channels=hidden_dim, out_channels=num_channels, kernel_size=kernel_size[1]),
+            nn.ReLU(),
+            nn.MaxPool1d(max_len - kernel_size[1]+1)
+        )
+        self.conv3 = nn.Sequential(
+            nn.Conv1d(in_channels=hidden_dim, out_channels=num_channels, kernel_size=kernel_size[2]),
+            nn.ReLU(),
+            nn.MaxPool1d(max_len - kernel_size[2]+1)
+        )
+        
+        self.dropout = nn.Dropout(dropout)
+        
+        # Fully-Connected Layer
+        self.fc = nn.Linear(num_channels*len(kernel_size), num_classes)
+        
+    def forward(self, x):
+        if type(x) == type([]):
+            text, _ = x
+        else:
+            text = x
+
+        embedded_sent = self.embedding(text).permute(0,2,1)
+        
+        conv_out1 = self.conv1(embedded_sent).squeeze(2)
+        conv_out2 = self.conv2(embedded_sent).squeeze(2)
+        conv_out3 = self.conv3(embedded_sent).squeeze(2)
+        
+        all_out = torch.cat((conv_out1, conv_out2, conv_out3), 1)
+        final_feature_map = self.dropout(all_out)
+        out = self.fc(final_feature_map)
+
+        return out
